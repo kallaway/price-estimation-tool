e981e8b3059f2181c437fdd0414aaf91bc7a9804
Alexander Kallaway <kallaway@me.com>
2016-09-19 02:56:02 -0400

    setup initial structure for the tool, add testing frameworks as depenencies - mocha, chai

---
+++ test/pricing-test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+describe('convertPercentToDecimal', function() {
+	it('', function() {
+
+	});
+});
+
+// function to calculate price with base markup
+describe('');
+
+
+describe('estimatePrice', function() {});
